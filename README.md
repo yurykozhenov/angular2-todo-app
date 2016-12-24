4b558f1f01d62bf21fde6d037fc4d997a9c5bcdd
Yury Kozhenov <yurykozhenov@gmail.com>
2016-12-24 19:11:09 +0200

    Configure basic project structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# angular2-todo-app
+# [Angular 2 Todo app](https://yk-angular2-todo-app.herokuapp.com)
+### Basic Angular 2 application
+
+Requirements:
+- NodeJS >= 6.9.2
+
+Install dependencies
+```
+npm install
+```
+
+Run app in debug mode
+```
+npm start
+```
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at ${HOST}:${PORT}`);
+});
