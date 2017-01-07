9f1f0c49e5a7c63eac5edd9cc0985071d175ce88
Yury Kozhenov <yurykozhenov@gmail.com>
2017-01-08 00:27:23 +0200

    Update server.js in order for angular routing to work

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 app.listen(PORT, HOST, () => {
   console.log(`Server running at ${HOST}:${PORT}`);
 });
